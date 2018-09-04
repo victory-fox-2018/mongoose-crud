ef439c5f06903137916a41732a04b64f4c901b6c
windra <pwindrap18@gmail.com>
2018-09-04 21:36:23 +0700

    done

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -19,5 +19,72 @@ module.exports = {
                 })
             }
         })
+    },
+
+    updateTransaction : function(req,res) {
+        let returnDate = new Date
+        returnDate = returnDate.setDate(returnDate.getDate()+7)
+        console.log(req.params.id)
+        Transaction.update({ _id: new ObjectId(req.params.id) },{
+            $set : {
+                in_date: new Date(returnDate),
+                fine: 1000
+            }
+        },function(err,data){
+            if (err) {
+                res.status(500).json({
+                    message: err.message
+                })
+            } else {
+                res.status(200).json({
+                    transaction : data,
+                    message: 'success'
+                })
+            }
+        })
+    },
+
+    getAllTransaction : function(req,res) {
+        Transaction.find(function(err,data){
+            if (err) {
+                res.status(500).json({
+                    message : err.message
+                })
+            } else {
+                Transaction.populate(data,{path:'member'},function(err,trans){
+                    if (err) {
+                        res.status(500).json({
+                            message: err.message
+                        })
+                    } else {
+                        Transaction.populate(trans,{path:'booklist'},function(err,dataTrans){
+                            if (err){
+                                res.status(500).json({
+                                    message : err.message
+                                })
+                            } else {
+                                res.status(200).json({
+                                    data : dataTrans
+                                })
+                            }
+                        })
+                    }
+                })
+            }
+        })
+    },
+
+    deleteTransaction : function(req,res) {
+        Transaction.remove({_id: new ObjectId(req.params.id)},function(err){
+            if (err) {
+                res.status(500).json({
+                    message : err.message
+                })
+            } else {
+                res.status(200).json({
+                    message: 'success'
+                })
+            }
+        })
     }
 }
--- models/transaction.js
+++ models/transaction.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const transactionSchema = new Schema({
-    member: { type: Schema.Types.ObjectId, ref: 'Cusstomer' },
+    member: { type: Schema.Types.ObjectId, ref: 'Customer' },
     days: Number,
     out_date: { type : Date, default: new Date },
     due_date: Date,
-    in_date: { type : Date, default: null },
+    in_date: Date,
     fine: Number,
     booklist: [{ type: Schema.Types.ObjectId, ref: 'Book' }]
 })
