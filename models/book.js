75161a59dfe0a416c20decda502e70f44573af3e
harlesbayu <bayuanggaraharles@gmail.com>
2018-09-04 23:21:23 +0700

    add validation

--- models/book.js
+++ models/book.js
@@ -2,11 +2,26 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const bookSchema = new Schema({
-    isbn:  String,
-    title: String,
-    author:   String,
-    category: String,
-    stock: Number,
+    isbn: {
+        type: String,
+        required: [true, 'is required']
+    },
+    title: {
+        type: String,
+        required: [true, 'is required']
+    },
+    author:   {
+        type: String,
+        required: [true, 'is required']
+    },
+    category: {
+        type: String,
+        required: [true, 'is required']
+    },
+    stock: {
+        type: Number,
+        required: [true, 'is required']
+    },
 }, {
     timestamps : true
 })
--- models/customer.js
+++ models/customer.js
@@ -2,14 +2,44 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const customerSchema = new Schema({
-    name:  String,
-    memberid: String,
-    address:   String,
-    zipcode: Number,
-    phone: Number,
+    name:  {
+        type: String,
+        required: [true, 'is required']
+    },
+    memberid: {
+        type: String,
+        required: [true, 'is required']
+    },
+    address: {
+        type: String,
+        required: [true, 'is required']
+    },
+    zipcode:{
+        type: Number,
+        required: [true, 'is required']
+    },
+    phone: {
+        type: String,
+        validate: {
+        validator: function(v) {
+            return /\d{3}-\d{3}-\d{4}/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    }
 }, {
     timestamps : true
 })
+
+// var mongooseIntlPhoneNumber = require('mongoose-intl-phone-number');
+ 
+// customerSchema.plugin(mongooseIntlPhoneNumber, {
+//     hook: 'validate',
+//     phone: 'phoneNumber',
+//     phone: 'nationalFormat',
+//     phone: 'internationalFormat'
+// });
 
 const Customer = mongoose.model('Customer', customerSchema)
 
