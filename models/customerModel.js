a2c65d0d9d8e1e8486290c6bda8abaadaf4a8f51
talkasrul <talkasrul@gmail.com>
2018-09-04 14:30:57 +0700

    Hidden version key

--- models/customerModel.js
+++ models/customerModel.js
@@ -9,7 +9,16 @@ const customer = new Schema({
     memberid: String,
     address: String,
     zipcode: String,
-    phone: String
+    phone: {
+        type: String,
+        validate: {
+            validator: function(v) {
+                return /\d{4}-\d{4}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'User phone number required']
+    }
 },{
     timestamps:true     
 });
--- models/transactionModel.js
+++ models/transactionModel.js
@@ -4,12 +4,13 @@ const Schema = mongoose.Schema;
 const transaction = new Schema({ 
     member: { type: Schema.Types.ObjectId, ref: 'Customer'},
     days: Number,
-    out_date: Date,
+    out_date: {type: Date, default: Date.now},
     in_date: Date,
     fine: Number,
     booklist: [{ type: Schema.Types.ObjectId, ref: 'Book'}]
 },{
-    timestamps:true     
+    timestamps:true,
+    versionKey: false     
 });
 
 
