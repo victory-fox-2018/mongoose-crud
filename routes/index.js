fc8ba63a1c935196d80ce8917f68e46e6844cc5c
talkasrul <talkasrul@gmail.com>
2018-09-04 15:31:28 +0700

    CRUD Customer

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const book = require('./bookRouter')
+const customer = require('./customerRouter')
 
 router.use('/api/books', book)
+router.use('/api/customers', customer)
 
 router.get('/', (req, res, next) => {
     res.status(200).json("REST API ASRUL H")
