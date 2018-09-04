653c68ee713fe9e360eaf9d2b098b2f886321beb
talkasrul <talkasrul@gmail.com>
2018-09-04 15:11:58 +0700

    CRUD Books is done

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 const router = require('express').Router()
+const book = require('./bookRouter')
+
+router.use('/api/books', book)
 
 router.get('/', (req, res, next) => {
     res.status(200).json("REST API ASRUL H")
